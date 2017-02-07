968800bd2eae0296a63cae87b5854a208cc1501c
daartv <daartv@gmail.com>
2017-02-06 16:06:19 -0800

    all tests passed for ElmentByclassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,28 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
   // your code here
-};
+  var getElementsByClassName = function(className) {
+    // instantiate a return array
+    var result = [];
+  // your code here
+    // create a function
+    // take a complete element as input
+    var helper = function (node) {
+        // compare those class name(s) to the original parameter
+      if (node.classList.contains(className)) {
+          // if they match
+          // push it to the results array
+        result.push(node);
+      } 
+      // get to the lower level children 
+      var children = node.children;
+      // execute the function with every child as input
+      for (var j = 0; j < children.length; j++) {
+        helper(children[j]);
+      }
+    };
+    helper(document.body);
+    return result;
+  };
